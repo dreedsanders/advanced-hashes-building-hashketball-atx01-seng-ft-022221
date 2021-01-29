0925aab990ad75a795e3ee6259166b584542841d
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-29 23:54:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
 def game_hash
   game_hash = {}
-  game_hash = {:home => {}, :away => {}}
+  game_hash = {:home => {:team_name => "Brooklyn Nets", :colors =>[], :players => [{}]}, :away => {:team_name =>"", :colors => [], :players => [{}]}}
 end 
