99d9959c33d7ef84b7ca58c1b09d367c16d2e8c2
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-30 00:23:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,17 +10,16 @@ def game_hash
   :assists => 12, 
   :steals => 3, 
   :blocks => 1,
-  :slam_dunks => 1]}}, 
+  :slam_dunks => 1}]}, 
   :away => {:team_name =>"", 
   :colors => [], 
-  :players => [{:player_name => ""},
-  {:number => ""}, 
-  {:shoe => ""},
-  {:points => ""}, 
-  {:rebounds => ""}, 
-  {:assists => ""}, 
-  {:steals => ""}, 
-  {:blocks => ""},
-  {:slam_dunks => ""}
-  ]}}
+  :players => [{:player_name => "",
+  :number => "", 
+  :shoe => "",
+  :points => "", 
+  :rebounds => "", 
+  :assists => "", 
+  :steals => "", 
+  :blocks => "",
+  :slam_dunks => ""}]}}
 end 
