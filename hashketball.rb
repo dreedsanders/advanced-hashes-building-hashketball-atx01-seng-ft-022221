0f5443b59ed1d4ea51c4faad2ef3e3fbbe993f4b
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-30 00:15:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,14 +3,14 @@ def game_hash
   {:team_name => "Brooklyn Nets", 
   :colors =>["Black", "White"], 
   :players => [{:player_name => "Alan Anderson"},
-  :number => "0", 
-  :shoe => "16",
-  :points => 22, 
-  :rebounds => 12, 
-  :assists => 12, 
-  :steals => 3, 
-  :blocks => 1,
-  :slam_dunks => 1]}, 
+  {:number => "0"}, 
+  {:shoe => "16"},
+  {:points => 22}, 
+  {:rebounds => 12}, 
+  {:assists => 12}, 
+  {:steals => 3}, 
+  {:blocks => 1},
+  {:slam_dunks => 1]}, 
   :away => {:team_name =>"", 
   :colors => [], 
   :players => [{:player_name => "", },
@@ -20,5 +20,12 @@ def game_hash
   {:rebounds => ""}, 
   {:assists => ""}, 
   {:steals => ""}, 
-  {:blocks => ""}]}}
+  {:blocks => ""},
+  {:slam_dunks => ""}
+  ]
+    
+  }
+    
+  }
+  }
 end 
